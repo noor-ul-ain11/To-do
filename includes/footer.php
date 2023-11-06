2a6d2ddc1c3f540bb96acf4bc7b364703dfea169
Noor ul Ain Abid <143778981+noor-ul-ain11@users.noreply.github.com>
2023-11-06 16:23:59 +0500

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+</div>
+<footer class="bg-light text-center text-lg-start" style="position: absolute; bottom: 0; width:100%;">
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    All Rights  Reserved © 2023 Copyright:
+    <!-- <a class="text-dark">mywebsite.com</a> -->
+  </div>
+  <!-- Copyright -->
+</footer>
+</body>
+</html>
